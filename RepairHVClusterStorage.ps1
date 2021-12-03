332f330df3dd4afa5e53cc64555cdb93462f5334
unknown <Administrator@WINADMIN.LOCAL>
2021-12-04 00:59:53 +0530

    test

---
+++ RepairHVClusterStorage.ps1
@@ -0,0 +1,25 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2020 v5.7.172
+	 Created on:   	04-12-2021 00:29
+	 Created by:   	venu
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+
+# Repair storage
+
+Get-Cluster
+
+Get-PhysicalDisk
+
+Get-VirtualDisk
+
+Get-StorageJob
+
+Get-Service -Name ClusSvc
